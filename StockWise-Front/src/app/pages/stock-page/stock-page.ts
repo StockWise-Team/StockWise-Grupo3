5b35baf032e26e0ddbb9e37ad07624da347dd604
Iara Evelyn Baya Vargas <iarabayav@gmail.com>
2025-11-12 03:46:13 -0300

    feat: agrego consulta y armado tabla stock

--- StockWise-Front/src/app/pages/stock-page/stock-page.ts
+++ StockWise-Front/src/app/pages/stock-page/stock-page.ts
@@ -1,28 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, effect, OnInit, signal } from '@angular/core';
 import { Stock } from '@app/models/stock.model';
 import { StockService } from '@app/services/stock.service';
+import { GenericTable } from '@app/shared/generic-table/generic-table';
+import { StockTable } from "@app/components/stock-table/stock-table";
+import { Observable } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-stock-page',
-  imports: [],
+  imports: [ StockTable, AsyncPipe],
   templateUrl: './stock-page.html',
   styleUrl: './stock-page.css',
 })
 export class StockPage {
-  stockList: Stock[] = [];
+  stockList$!: Observable<Stock[]>;
 
-  constructor(private _stockService: StockService) {}
 
-  OnInit(): void {
-    console.log('inicio stock page');
-    this.getStockList();
-  }
-
-  getStockList() {
-    console.log('consulta servicio stock')
-    this._stockService.loadStockList().subscribe({
-      next: (data) => (this.stockList = data),
-      error: (err) => console.log(err),
-    });
+  constructor(private _stockService: StockService) {
+    effect(() => {
+      this.stockList$ = this._stockService.loadStockList();
+    })
   }
 }
